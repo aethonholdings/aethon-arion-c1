1ac9f9380dcddebebc6aa80eabdf90571575b514
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2025-04-06 19:03:02 +0100

    feat:convergece testing of OptimiserSteps

--- src/classes/optimisers/c1-gradient-ascent.optimiser.class.ts
+++ src/classes/optimisers/c1-gradient-ascent.optimiser.class.ts
@@ -39,7 +39,7 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
     }
 
     initialise(simSetDTO: SimSetDTO): OptimiserStateDTO<GradientAscentOptimiserData<C1ConfiguratorParamData>> {
-        return this._step(true, undefined, undefined, simSetDTO);
+        return this._step(true, undefined, simSetDTO);
     }
 
     step(
@@ -47,7 +47,7 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
         resultsDTO?: ConvergenceTestDTO[]
     ): OptimiserStateDTO<GradientAscentOptimiserData<C1ConfiguratorParamData>> {
         if (stateDTO && resultsDTO) {
-            return this._step(false, stateDTO, resultsDTO);
+            return this._step(false, stateDTO);
         } else {
             throw new Error("Optimiser step requires a state and resultDTO");
         }
@@ -59,42 +59,61 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
     ): OptimiserStateDTO<GradientAscentOptimiserData<C1ConfiguratorParamData>> {
         // check if all results required by the state have been received
         if (state && results) {
-            // check if the state is in a valid state
+            // find the x data point that the gradient ascent is based on
             const x = state.optimiserData.dataPoints.find((dataPoint) => dataPoint.id === "x");
+            // find the simulation convergence test result corresponding to x because
+            // we will need to access the current estimate of simulation performance
             const xResult = results.find((result) => result.configuratorParams.hash === x?.data.inputs.hash);
+
             let completed: boolean = true;
             let running: boolean = false;
             let failed: boolean = false;
-            let found: boolean = true;
+
+            // cycle through all the required data points for gradient descent and find the corresponding
+            // simulation ConvergenceTest result.  Then calculate the current estimated outputs for each
             for (let requiredDataPoint of state.optimiserData.dataPoints) {
                 const result = results.find(
                     (result) => result.configuratorParams.hash === requiredDataPoint.data.inputs.hash
                 );
-                const domain = this._parameterDomains[requiredDataPoint.id];
-                if (!result) found = false;
+                if (!result) throw new Error(`Not all results were received for optimiserStateId: ${state.id}`);
                 else {
+                    const domain = this._parameterDomains[requiredDataPoint.id];
+                    requiredDataPoint.status = result.state;
                     if (requiredDataPoint.id === "x") state.performance = result.avgPerformance;
-                    if (result.state !== States.COMPLETED) completed = false;
-                    if (result.state === States.RUNNING) running = true;
-                    if (result.state === States.FAILED) failed = true;
-                    if (result.state === States.COMPLETED || result.state === States.RUNNING) {
-                        requiredDataPoint.status = result.state;
-                        let prevPerformance = requiredDataPoint.data.outputs.performance
-                            ? requiredDataPoint.data.outputs.performance
-                            : result.avgPerformance;
-                        requiredDataPoint.data.outputs.performance = result.avgPerformance;
+                    // determine the state of overall optimisation step flags based on the state
+                    // of the result data points
+                    if (requiredDataPoint.status !== States.COMPLETED) completed = false;
+                    if (requiredDataPoint.status === States.RUNNING) running = true;
+                    if (requiredDataPoint.status === States.FAILED) failed = true;
+
+                    // for all parameters, update the output performance value
+                    // store the current value in prevPerformance before doing so
+                    // as it will be needed to assess which category is showing improved performance
+                    // for categorical variable domains
+                    let prevPerformance = requiredDataPoint.data.outputs.performance
+                        ? requiredDataPoint.data.outputs.performance
+                        : result.avgPerformance; // store the previous value; we will need it
+                    requiredDataPoint.data.outputs.performance = result.avgPerformance; // update to the current result value
+
+                    if (
+                        requiredDataPoint.id !== "x" &&
+                        (requiredDataPoint.status === States.COMPLETED || result.state === States.RUNNING)
+                    ) {
+                        // for categorical domain parameters, update the output next step category
+                        // maximising performance
                         if (
                             prevPerformance &&
                             requiredDataPoint.data.outputs.performance &&
                             requiredDataPoint.data.outputs.performance > prevPerformance &&
-                            this._parameterDomains[requiredDataPoint.id] &&
-                            (this._parameterDomains[requiredDataPoint.id].type === DomainTypes.CATEGORICAL ||
-                                this._parameterDomains[requiredDataPoint.id].type === DomainTypes.BOOLEAN)
+                            domain &&
+                            (domain.type === DomainTypes.CATEGORICAL || domain.type === DomainTypes.BOOLEAN)
                         ) {
                             requiredDataPoint.data.outputs.configuratorParameterValue = flatten(
                                 result.configuratorParams.data
                             )[requiredDataPoint.id];
                         }
+                        // for all parameters, update the performance delta to the x point
+                        // as well as the slope, where applicable (where an xDelta exists)
                         if (xResult) {
                             if (requiredDataPoint.id !== "x") {
                                 requiredDataPoint.data.outputs.performanceDelta =
@@ -109,17 +128,20 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
                     }
                 }
             }
-            if (!found) throw new Error("Not all results were received");
+            // check the completed flag to see if all required data point result convergence test
+            // were completed; in this case, all results for the state have been fully calculated
+            state.converged = false;
             if (completed) {
-                // state.status = States.COMPLETED;
-                // state.converged = this._testConvergence(state.optimiserData.dataPoints);
-                // state.percentComplete = 100;
-            }
-            if (running) {
+                state.status = States.COMPLETED;
+                state.percentComplete = 100;
+                state.end = new Date();
+                state.durationSec = state.start ? (state.end.getTime() - state.start.getTime()) / 1000 : undefined;
+                state.converged = this._testConvergence(state);
+            } else if (running) {
                 state.status = States.RUNNING;
-            }
-            if (failed) {
+            } else if (failed) {
                 state.status = States.FAILED;
+                state.percentComplete = undefined;
                 throw new Error("Optimiser state update failed; one or more results failed");
             }
         } else {
@@ -140,33 +162,40 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
     private _step(
         init: boolean,
         state?: OptimiserStateDTO<GradientAscentOptimiserData<C1ConfiguratorParamData>>,
-        results?: ConvergenceTestDTO[],
         simSetDTO?: SimSetDTO
     ): OptimiserStateDTO<GradientAscentOptimiserData<C1ConfiguratorParamData>> {
         let newState: OptimiserStateDTO<GradientAscentOptimiserData<C1ConfiguratorParamData>> = {} as OptimiserStateDTO<
             GradientAscentOptimiserData<C1ConfiguratorParamData>
         >;
 
-        // check if this is an initialisation step
-        if (init && simSetDTO) {
-            newState = {
-                modelName: this.model.name,
-                optimiserName: this.name,
-                converged: false,
-                percentComplete: 0,
-                status: States.PENDING,
-                stepCount: 0,
-                simSet: simSetDTO,
-                optimiserData: {}
-            } as OptimiserStateDTO<GradientAscentOptimiserData<C1ConfiguratorParamData>>;
-            newState.optimiserData.dataPoints = [];
-
-            // it is, pick a random configuration within the configuration parameter space as the
-            // initial x data point
-            newState.optimiserData.dataPoints.push(this._getNewDataPoint("x", this._getRandomInit()));
+        // check if this is the first simulation step
+        if (init) {
+            if (simSetDTO) {
+                // this is the first step, so stepCount is zero;
+                // initialise the first OptimisationStep
+                newState = {
+                    modelName: this.model.name,
+                    optimiserName: this.name,
+                    converged: false,
+                    start: new Date(),
+                    percentComplete: 0,
+                    status: States.PENDING,
+                    stepCount: 0,
+                    simSet: simSetDTO,
+                    optimiserData: {
+                        // pick a random configuration within the configuration parameter space as the
+                        // initial x data point
+                        dataPoints: [this._getNewDataPoint("x", this._getRandomInit())]
+                    }
+                } as OptimiserStateDTO<GradientAscentOptimiserData<C1ConfiguratorParamData>>;
+            } else {
+                throw new Error("Initialisation of Optimiser requires a simSetDTO");
+            }
         }
-        if (!init && state && results) {
-            // we are stepping out of an existing state
+        // this is not the first optimisation step
+        else if (!init && state && state.status === States.COMPLETED) {
+            // we are stepping out of an existing state, so copy the basic key variables from the existing
+            // state object passed to this function
             newState = {
                 stepCount: state.stepCount + 1,
                 simSet: state.simSet,
@@ -184,11 +213,14 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
             let xPrev: DataPoint<ConfiguratorParamsDTO<C1ConfiguratorParamData>, GradientAscentOutput> | undefined =
                 state.optimiserData.dataPoints.find((dataPoint) => dataPoint.id === "x");
             if (xPrev) {
-                // flatten the xConfig for the x piont for easy reference
+                // flatten the configurator parameters used for the x poimt for easy reference
                 let currentXConfigFlat = flatten(JSON.parse(JSON.stringify(xPrev.data.inputs.data)));
-                // copy the current x config to a new object to use for the new x
+                // copy the current x configurator parameters to a new object to use
+                // for the new x that we are stepping to
                 let newXConfigFlat = JSON.parse(JSON.stringify(currentXConfigFlat));
 
+                // cycle through all parameters and, for the ones that we are optimising against,
+                // proceed to calculate the next step
                 for (const key in this._parameterDomains) {
                     const domain = this._parameterDomains[key];
                     if (domain.optimise) {
@@ -271,9 +303,13 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
                     let xConfiguratorNewValue;
                     let xDelta: number = 0;
                     let xPlusXDelta: number = 0;
-                    
+
+                    // determine the new x
+                    // for discrete and continuous domains
                     if (domain.type === DomainTypes.DISCRETE || domain.type === DomainTypes.CONTINUOUS) {
                         xPlusXDelta = xConfiguratorCurrentValue;
+                        // if we have hit the domain bounds, step backwards so that we still get a slope estimate
+                        // at the boundary.  xDelta will be a negative number
                         if (xConfiguratorCurrentValue < domain.max) {
                             xDelta = Math.min(domain.derivativeStepSize, domain.max - xConfiguratorCurrentValue);
                         } else {
@@ -282,6 +318,7 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
                         xPlusXDelta += xDelta;
                         xConfiguratorNewValue = xPlusXDelta;
                     }
+                    // for boolean domains, just flip the flag of the input
                     if (domain.type === DomainTypes.BOOLEAN) {
                         xDelta = xConfiguratorCurrentValue ? -1 : 1;
                         xPlusXDelta += xDelta;
@@ -300,7 +337,7 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
                     del.push(this._getNewDataPoint(key, xPlusDxConfigParams, xPlusDxOutput));
                 }
                 // in categorical domains, we will need to get data points for each category in order to figure out
-                // which direction to move in the gradient ascent
+                // which to which category to move in the gradient ascent
                 if (domain.type === DomainTypes.CATEGORICAL) {
                     let currentCategoryIndex = domain.categories.indexOf(xConfiguratorCurrentValue);
                     for (let i = 0; i < domain.categories.length; i++) {
@@ -323,10 +360,49 @@ export class C1GradientAscentOptimiser extends GradientAscentOptimiser<
         return del;
     }
 
-    private _testConvergence(
-        gradient: DataPoint<ConfiguratorParamsDTO<C1ConfiguratorParamData>, GradientAscentOutput>[]
-    ): boolean {
-        return false;
+    // test convergence by taking the modulo of the del
+    private _testConvergence(state: OptimiserStateDTO<GradientAscentOptimiserData<C1ConfiguratorParamData>>): boolean {
+        let modulo: number = 0;
+
+        for (let dataPoint of state.optimiserData.dataPoints) {
+            // for all parameters (exclude x)...
+            if (dataPoint.id !== "x") {
+                const domain = this._parameterDomains[dataPoint.id];
+                if (domain && domain.optimise) {
+                    // for the subset of parameters where we are performing optimisation, there will be components
+                    // to the del
+                    if (
+                        (domain.type === DomainTypes.CONTINUOUS || domain.type === DomainTypes.DISCRETE) &&
+                        dataPoint.data.outputs.slope
+                    ) {
+                        // for numerical domains, the contribution to the modulo of the del will be a straightforward
+                        // square of the slope, unless the variable is at the domain maximum, in which case contribution
+                        // will only be made if the slope is negative in which case the gradient ascent can proceed within the
+                        // domain
+                        if (!(dataPoint.data.outputs.xPlusDelta === domain.max && dataPoint.data.outputs.slope > 0))
+                            modulo += dataPoint.data.outputs.slope ** 2;
+                    } else if (
+                        (domain.type === DomainTypes.BOOLEAN || domain.type === DomainTypes.CATEGORICAL) &&
+                        dataPoint.data.outputs.performanceDelta
+                    ) {
+                        // for categorical and boolean domains, if the expected performance change is not positive,
+                        // then there is no further scope for optimisation; therefore the contribution to the del is
+                        // the expected performance change along the optimisation dimension, otherwise it is zero
+                        // not taking a square here to avoid overflow due to hight numbers
+                        if (dataPoint.data.outputs.performanceDelta > 0)
+                            modulo += dataPoint.data.outputs.performanceDelta;
+                    }
+                } else {
+                    throw new Error(
+                        `Domain ${domain} not found or insufficient output data to test convergence for optimiser state id:${state.id}`
+                    );
+                }
+            }
+        }
+        // take the square root of the modulo
+        modulo = Math.sqrt(modulo);
+        // check against the tolerance of the optimisation for convergence
+        return modulo < this._parameters.iterations.tolerance;
     }
 
     protected _boundRandom(min: number, max: number): number {
